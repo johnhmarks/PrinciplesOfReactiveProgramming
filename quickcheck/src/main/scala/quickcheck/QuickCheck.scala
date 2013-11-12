8477f1437e958e68acb72ba496249dbe25f13a36
John Marks <john@johnmarks.co.uk>
2013-11-12 14:23:37 +0000

    All tests pass

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,26 +19,40 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h))==m
   }
 
-  property("minimum of two elements is smallest") = forAll { (a: Int, b: Int) =>
+  property("minimum of two elements is smallest") = forAll { (a: A, b: A) =>
     val h = insert(a, insert(b, empty))
     findMin(h) ==  (a min b)
   }
 
-  property("delete single elemnt from heap is empty") = forAll { (a: Int) =>
+  property("delete single elemnt from heap is empty") = forAll { (a: A) =>
     val h = deleteMin(insert(a, empty))
     isEmpty(h)
   }
 
-  /*
   property("finding and deleting minima is ordered") = forAll { (h: H) =>
-
+    isOrdered(popAll(h))
   }
-  */
 
   property("minimum of melded heaps should be minimum of both inputs") = forAll { (l: H, r: H) =>
     val melded = meld(l, r)
     val minimum = (findMin(l) min findMin(r))
     findMin(melded) == minimum
+  }
+
+  property("melded heap contains all elements of inputs") = forAll { (l: H, r: H) =>
+    val expected = (popAll(l) ++ popAll(r)).sorted
+    val actual = popAll(meld(l, r)).sorted
+    expected == actual
+  }
+
+  def popAll(h: H): List[A] = {
+    if (isEmpty(h)) List() else findMin(h) :: popAll(deleteMin(h))
+  }
+
+  def isOrdered(as: List[A]): Boolean = as match {
+    case Nil => true
+    case _ :: Nil => true
+    case b :: bs => if (b <= bs.head) isOrdered(bs) else false
   }
 
   lazy val genHeap: Gen[H] = for {
